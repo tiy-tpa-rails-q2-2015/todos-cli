f11b2ac5580fc87cf031d0dc7bb531fcecbdef7b
Josh Wykell <jwykell@gmail.com>
2015-06-04 11:21:51 -0400

    created add item

--- lib/todo/cli.rb
+++ lib/todo/cli.rb
@@ -7,9 +7,22 @@ require 'list'
 module Todo
   module CLI
     
-    def self.add(list_name, list_item)
-      
-      
+    # Adds a new item to a list.
+    #If the list does not exist it is automatically created.
+    def self.add(list_name, list_item) 
+      #find list by name.
+      list = List.find_by name: list_name
+      #if it does not exist
+      unless list # or if !list
+        #create a list.
+        list = List.create name: list_name
+        puts "Created list: #{list.name}."
+      else
+        puts "Found list #{list.name}."
+      end
+      puts "Creating list item."
+      #create a list item.
+                                       
     end
 
     def self.run
